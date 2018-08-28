13de7b9e0d1ed15355b89acbfb3b21f06c0f4cf0
clau1889 <clau.blanco1889@gmail.com>
2018-08-27 22:13:04 -0500

    En este commit se sube diseño de página principal para mostrar estudiantes y se crea un array de objetos

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('.chips-placeholder').chips({
+        placeholder: 'Add your skill',
+        secondaryPlaceholder: '+tag',
+    });      
+
+  });
---
+++ js/data-students.js
@@ -0,0 +1,36 @@
+var data = {
+    'students': [
+        {
+            'name': 'Samira Morales Estrada',
+            'photo': 'https://static.vix.com/es/sites/default/files/styles/large/public/1/10-mandamientos-del-maquillaje-natural-que-toda-mujer-deberia-conocer4.jpg?itok=FPAm1_Xw'
+
+        },
+        {
+            'name': 'Fátima Silva Trujillo',
+            'photo': 'https://cdn.glamour.mx/uploads/images/thumbs/mx/glam/2/s/2017/28/las_carreras_mejor_pagadas_para_mujeres__5443_980x560.jpg'
+        },
+
+        {
+            'name': 'Luz María Gónzalez Móntes',
+            'photo': 'http://www.wradio.com.co/dmz/programas/imagenes/500x500_753879.png'
+        },
+        {
+            'name': 'Laura Angélica Rosales Montiel',
+            'photo': 'https://www.rizog.com/wp-content/uploads/2018/03/cortes-de-pelo-largos-para-mujeres-delgadas.jpg'
+        
+        },
+        {
+            'name': 'Sandra Lucía Castellanos Ruíz',
+            'photo': 'https://static.paraloscuriosos.com/img/articles/20629/800x800/5a5c7d9fadf95_mujer_1.jpg'
+        }
+        {
+            'name': 'Fabiola Sandoval Torres',
+            'photo': 'https://www.eljueves.es/medio/2018/03/08/mujer-1_d7b61f7d_850x566.jpeg'
+
+        },
+        {
+            'name': 'Yadhira Tapia de la O',
+            'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqYh1Dfhh5lb2n1jBVNNctFbzPNP0Q8MDptIglpSUvQ4Videtd'
+        },
+    ]
+}
